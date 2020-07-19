dec6e5e711b6f6e44eb2b0a0438574026e1cef41
Yamid Velez Muñoz <yamid.velez@arkixsa.com>
2020-07-18 22:50:17 -0500

    my first page with styles inside

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 const Home = () => {
-  return ( <h1>HOME!!!</h1> );
+  return ( 
+    <div>
+      <h1>Welcome!!</h1>
+      <p>This is my default page and it has its own styles</p>
+
+      <style jsx>{`
+        h1 {
+          color: grey
+        }
+
+        p {
+          color: red
+        }
+      `}</style>
+    </div> 
+  );
 }
  
 export default Home;
