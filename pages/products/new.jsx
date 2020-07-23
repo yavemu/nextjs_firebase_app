1c16c3c7a17fbd6ecc2318aad37528d9df8329e1
Yamid Velez Muñoz <yamid.velez@arkixsa.com>
2020-07-22 23:31:39 -0500

    Create new product form and validations

--- pages/products/new.jsx
+++ pages/products/new.jsx
@@ -1,11 +1,121 @@
+import React, { useState } from 'react';
+import Router from 'next/router'
+
+import { Title } from '../../components/ui/styles/Utils'
+import { Form, Field, InputSubmit, FieldError } from '../../components/ui/styles/Form'
 import Layout from '../../components/layout/Layout'
+import useValidation from '../../hooks/useValidation'
+import productValidations from '../../validationRules/productValidations'
+import firebase from '../../firebase'
+
+const INITIAL_STATE = {
+  product_name: '',
+  business_name: '',
+  image: '',
+  url: '',
+  description: '',
+}
 
 const NewProduct = () => {
-  return ( 
-    <Layout>
-      <h1>NewProduct</h1>
-    </Layout> 
-  );
+  const [errors, setErrors] = useState()
+    
+    const handleNewProduct =  async () => {
+        try {
+            setErrors('')
+            await firebase.login(product_name, business_name, image, url, description)
+            Router.push('/')
+        } catch (error) {
+            setErrors(error.message)
+        }
+    }
+    
+    const {valuesSaved,errorsValidation,handleSubmit,handleChange, handleValidate} = useValidation(INITIAL_STATE,productValidations, handleNewProduct)
+    const { product_name, business_name, image, url, description } = valuesSaved
+
+    return ( 
+        <Layout>
+        <Title>New Product</Title>
+        <Form onSubmit={handleSubmit} noValidate>
+          <fieldset>
+            <legend>General Information</legend>
+            <Field>
+                <label htmlFor='product_name'>Product Name</label>
+                <input  type="text" 
+                        id='product_name' 
+                        name='product_name'
+                        placeholder='Product name'
+                        value={product_name}
+                        onChange={handleChange}
+                        onBlur={handleValidate}
+                        onKeyUp={handleValidate}
+                />
+            </Field>
+            {!!errorsValidation.product_name && <FieldError>{errorsValidation.product_name}</FieldError> }
+            <Field>
+                <label htmlFor='business_name'>Business name</label>
+                <input  type="text" 
+                        id='business_name' 
+                        name='business_name'
+                        placeholder='Business name'
+                        value={business_name}
+                        onChange={handleChange}
+                        onBlur={handleValidate}
+                        onKeyUp={handleValidate}
+                />
+            </Field>
+            {!!errorsValidation.business_name && <FieldError>{errorsValidation.business_name}</FieldError> }
+            <Field>
+                <label htmlFor='image'>Image</label>
+                <input  type="file" 
+                        id='image' 
+                        name='image'
+                        value={image}
+                        onChange={handleChange}
+                        onBlur={handleValidate}
+                        onKeyUp={handleValidate}
+                />
+            </Field>
+            {!!errorsValidation.image && <FieldError>{errorsValidation.image}</FieldError> }
+            <Field>
+                <label htmlFor='url'>Url</label>
+                <input  type="url" 
+                        id='url' 
+                        name='url'
+                        placeholder='Product url'
+                        value={url}
+                        onChange={handleChange}
+                        onBlur={handleValidate}
+                        onKeyUp={handleValidate}
+                />
+            </Field>
+            {!!errorsValidation.url && <FieldError>{errorsValidation.url}</FieldError> }
+          </fieldset>
+          <fieldset>
+            <legend>About your product</legend>
+            <Field>
+                <label htmlFor='description'>Description</label>
+                <textarea  
+                        id='description' 
+                        name='description'
+                        placeholder='description'
+                        value={description}
+                        onChange={handleChange}
+                        onBlur={handleValidate}
+                        onKeyUp={handleValidate}>
+
+                </textarea>
+            </Field>
+            {!!errorsValidation.description && <FieldError>{errorsValidation.description}</FieldError> }
+          </fieldset>
+            <Field>
+                <InputSubmit type="submit" value='Create new product'/>
+            </Field>
+            <Field>
+                {!!errors && <FieldError>{errors}</FieldError> }
+            </Field>
+        </Form>
+        </Layout> 
+    );
 }
  
 export default NewProduct;
---
+++ validationRules/productValidations.jsx
@@ -0,0 +1,29 @@
+const signupValidations = (values) => {
+    let errors = {};
+    const PRODUCT_NAME = values.product_name.trim();
+    const BUSINESS_NAME = values.business_name.trim();
+    const IMAGE = values.image.trim();
+    const URL = values.url.trim();
+    const DESCRIPTION = values.description.trim();
+
+    
+    //PRODUCT_NAME validations
+    if (!PRODUCT_NAME) {errors.product_name = 'Product name is required.'}
+    
+    //BUSINESS_NAME validations
+    if (!BUSINESS_NAME) {errors.business_name = 'Business name is required.'}
+    
+    //URL validations
+    if (!URL) {errors.url = 'Product url is required.'}
+    if(!!URL && !/^(ftp|http|https):\/\/[^ "]+$/.test(URL) ) {
+        errors.url = "Invalid url."
+    }
+    
+    //DESCRIPTION validations
+    if (!DESCRIPTION) {errors.description = 'Description is required.'}
+    
+    
+    return errors
+}
+
+export default signupValidations
