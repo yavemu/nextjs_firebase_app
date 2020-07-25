245d12de3e9c231908b5f19f22d2cbf5cc62a885
Yamid Velez Muñoz <yamid.velez@arkixsa.com>
2020-07-24 22:23:03 -0500

    get products from firebase

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,6 +1,30 @@
+import React, { useState, useEffect, useContext} from 'react';
 import Layout from '../../components/layout/Layout'
+import { FirebaseContext } from '../../firebase'
 
 const Products = () => {
+  const [products, setProducts] = useState([])
+  const { firebase } = useContext(FirebaseContext)
+
+  const handleSnapshot = (snapshot) => {
+    const products = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    })
+
+    setProducts(products);
+  }
+
+  const getProducts = () => {
+    firebase.db.collection('products').orderBy('createdDate','desc').onSnapshot(handleSnapshot)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return ( 
     <Layout>
       <h1>Products</h1>
